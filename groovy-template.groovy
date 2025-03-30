629e0302f10c3443e928b2e5c98e93b6463496a0
Sumanth M R <sumanthmr0103@gmail.com>
2025-03-30 22:14:24 +0530

    Updated code

--- groovy-template.groovy
+++ groovy-template.groovy
@@ -6,8 +6,8 @@ def reportPath = "test-output/emailable-report.html"
 // Read the file content
 def reportContent = new String(Files.readAllBytes(Paths.get(reportPath)))
 
-// Extract Summary Section
-def summarySection = reportContent.find(/<table.*?>.*?<\/table>/)
+// Extract Summary Section (Fixing the Regex)
+def summarySection = reportContent =~ /<table.*?>.*?<\/table>/s
 
 // Return Extracted Summary or Default Message
-return summarySection ?: "Test Summary Not Found!"
+return summarySection ? summarySection[0] : "Test Summary Not Found!"
